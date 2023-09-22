f64e52234f1a57bdd0b60af12ee82dca20d0bfe6
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-22 09:52:24 -0700

    WIP navbar

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -8,26 +8,36 @@ import {
 } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { shades } from "../../theme";
+import React from 'react';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   return (
+    
     <Box
     display="flex"
     alignItems="center"
     width="100"
     height="60px"
-    backgroundColor="#fffdd0"
     color="black"
     position="fixed"
     top="0"
     left="0"
     //so it appears above other divs
     zIndex="1"
-    ></Box>
-  )
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        ></Box>
+    </Box>
+
+  );
 };
 
 export default Navbar;
