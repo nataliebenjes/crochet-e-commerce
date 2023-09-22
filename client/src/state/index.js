7e157a302146217d4aa8251ea6ba18b2540e0804
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-22 10:36:09 -0700

    define a Redux slice and exports actions and a reducer

---
+++ client/src/state/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+//state which app will start with
+const initialState = {
+  isCartOpen: false,
+  cart: [],
+  items: [],
+}
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    //any time we have setItems, we can change the items to whatever we pass into the payload
+    setItems: (state, action) => {
+      state.items = action.payload;
+    }
+  }
+})
+
+export const {
+  setItems
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
