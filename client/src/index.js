4ee898929ae3ba8b0a128c37ae9e925b90224ee6
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-22 11:04:44 -0700

    add redux store to main index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,24 @@ import App from './App';
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
+//makes redux store available to whole component tree. No need to pass through props
+import { Provider } from "react-redux";
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from "./state";
 
+const store = configureStore({
+  reducer: { cart: cartReducer },
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      {/* //This means that any component within your app can now use styles defined in your theme. */}
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
