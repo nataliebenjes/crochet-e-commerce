538d339d11b2389afdf99fdd42508c451f691661
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-22 10:53:42 -0700

    complete redux state

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+To add... 
+edge case that prevents adding duplicate items in addToCart
+
+
 # 🚀 Getting started with Strapi
 
 Strapi comes with a full featured [Command Line Interface](https://docs.strapi.io/dev-docs/cli) (CLI) which lets you scaffold and manage your project in seconds.
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -14,12 +14,45 @@ export const cartSlice = createSlice({
     //any time we have setItems, we can change the items to whatever we pass into payload
     setItems: (state, action) => {
       state.items = action.payload;
+    },
+    addToCart: (state, action) => {
+      //take current state of cart, add whatever item you pass into action
+      state.cart = [...state.cart, action.payload.item];
+    },
+    removeFromCart: (state, action) => {
+      //anytime you want to remove from cart, keep all items that don't equal id that you're passing into filter
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+    },
+    increaseCount: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload.id) {
+          item.count++;
+        }
+        return item;
+      })
+    },
+    decreaseCount: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload.id && item.count > 1) {
+          item.count--;
+        }
+        return item;
+      })
+    },
+    setIsCartOpen: (state) => {
+      //flip watever state is
+      state.isCartOpen = !state.isCartOpen;
     }
   }
 })
 
 export const {
-  setItems
+  setItems,
+  addToCart,
+  removeFromCart,
+  increaseCount,
+  decreaseCount,
+  setIsCartOpen,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
