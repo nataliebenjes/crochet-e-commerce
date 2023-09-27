a7abcd3611b731d8853ea60a9983aeee083fe7ed
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-27 16:44:51 -0700

    display tabs. WIP api product call

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -1,6 +1,6 @@
 import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
-import "react-resoinsive-carousel/lib/styles/carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
@@ -99,4 +99,5 @@ const MainCarousel = () => {
     </Carousel>
   );
 };
+
 export default MainCarousel;
