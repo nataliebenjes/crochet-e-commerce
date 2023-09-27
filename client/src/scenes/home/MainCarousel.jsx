0a43704e726a444f1086ef679e4f4c9f0eecea15
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-27 11:25:18 -0700

    finish MainCarousel

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -4,6 +4,7 @@ import "react-resoinsive-carousel/lib/styles/carousel.min.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
+import React from "react";
 
 //imports all assets from folder
 const importAll = (r) =>
@@ -12,17 +13,90 @@ const importAll = (r) =>
     return acc;
   }, {});
 
-const heroTextureImports = importAll(
+export const heroTextureImports = importAll(
   require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
 );
+
 const MainCarousel = () => {
   //if above 600px, this is true
   const isNonMobile = useMediaQuery("(min-width:600px");
   return (
-    <Carousel>
-      
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      >
+        {/* grab all images and cycle through */}
+        {Object.values(heroTextureImports).map((texture, index) => (
+          <Box key={`carousel-image-${index}`}>
+            <img 
+              src={texture}
+              alt={`carousel-${index}`}
+              style={{
+                width: "100%",
+                height: "700px",
+                // makes responsive
+                objectFit: "cover",
+                backgroundAttachment: "fixed"
+              }}
+            />
+            {/* container for text */}
+            <Box
+              color="white"
+              padding="20px"
+              borderRadius="1px"
+              textAlign="left"
+              position="absolute"
+              top="46%"
+              left={isNonMobile ? "10%" : "0"}
+              right={isNonMobile ? undefined : "0"}
+              margin={isNonMobile ? undefined : "0 auto"}
+              maxWidth={isNonMobile ? undefined : "240px"}
+          >
+            <Typography color={shades.secondary[200]}>-- NEW ITEMS</Typography>
+            <Typography variant="h1">Summer Sale</Typography>
+            <Typography
+              fontWeight="bold"
+              color={shades.secondary[300]}
+              sx={{ textDecoration: "underline" }}
+            >
+              Discover More
+            </Typography>
+          </Box>
+        </Box>
+      ))}
     </Carousel>
-  )
-}
-
+  );
+};
 export default MainCarousel;
