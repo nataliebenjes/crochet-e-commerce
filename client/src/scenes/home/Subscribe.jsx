6bc9f6f085c01d28b849aa1b09326b28188bce56
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-28 14:56:22 -0700

    finish subscribe component

--- client/src/scenes/home/Subscribe.jsx
+++ client/src/scenes/home/Subscribe.jsx
@@ -11,10 +11,29 @@ const Subscribe = () => {
  return (
   <Box width="80%" margin="80px auto" textAlign="center">
     <IconButton>
-      <MarkEmailReadOutlinedIcon fontSize="large" />
+    <MarkEmailReadOutlinedIcon fontSize="large" />
     </IconButton>
     <Typography variant="h3">Subscribe to our Newsletter</Typography>
     <Typography>and receive 10% off your first order at checkout</Typography>
+    <Box
+      p="2px 4px"
+      m="15px auto"
+      display="flex"
+      alignItems="center"
+      width="75%"
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <Typography sx={{ p: "10px", ":hover": { cursor: "pointer" } }}>
+          Subscribe
+        </Typography>
+      
+      </Box>
   </Box>
  )
 }
