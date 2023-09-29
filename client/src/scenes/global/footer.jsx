a2248519e8711c03746b2dcf025ea2e708e7c802
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-29 11:23:17 -0700

    add imports to ItemDetails

--- client/src/scenes/global/footer.jsx
+++ client/src/scenes/global/footer.jsx
@@ -27,10 +27,32 @@ const Footer = () => {
           mb="30px"
           color={shades.secondary[500]}
           >Crochet store</Typography>
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores temporibus consectetur, ea impedit exercitationem laborum odit harum, totam molestias, voluptate illo quod molestiae dolores. Eveniet voluptas blanditiis eum asperiores quasi!
+          </div>
         </Box>
-
+        <Box>
+          <Typography variant="h4" fontWeight="bold"
+          mb="30px">
+            About us:
+          </Typography>
+          <Typography mb="30px">Careers</Typography>
+          <Typography mb="30px">FAQ</Typography>
+          <Typography mb="30px">Track Your Order</Typography>
+          <Typography mb="30px">Returns & Refund</Typography>
+        </Box>
+        <Box>
+          <Typography width="clamp(20%, 25%, 30%)">Contact Us</Typography>
+          <Typography mb="40px">5097 Store-address-in-PDX Portland, OR</Typography>
+          <Typography mb="30px">email: adventure.crochet@gmail.com</Typography>
+        </Box>
+        <Box>
+          <Typography width="clamp(20%, 25%, 30%)">Our Credo:</Typography>
+          <Typography mb="40px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta modi iste, eaque accusantium, placeat nesciunt quos dolor eligendi accusamus illum vero quisquam fuga obcaecati, laboriosam aliquam tempora ratione nobis amet!</Typography>
+        </Box>
       </Box>
   </Box>
+  )
 }
 
 export default Footer;
--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { IconButton, Box, Typography, useTheme, Button, Tabs } from "@mui/material";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+import { shades } from "../../theme.js";
+import { addToCart } from "../../state";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+//returns an object of key/value pairs of the dynamic params from  that were matched by the `<Route path>`
+import { useParams } from "react-router-dom";
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import Item from "../../components/item.jsx"
+
+
 const ItemDetails = () => {
-  return <div>ItemDetails</div>;
+  return <div>Item Details</div>;
 };
 
 export default ItemDetails;
