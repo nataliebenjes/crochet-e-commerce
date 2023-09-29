ae3a05416386822627486670bd49921e79bb6ceb
Natalie Benjes <natalie.benjes@gmail.com>
2023-09-29 13:22:18 -0700

    complete item details component setup

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from 'react-redux';
 import { IconButton, Box, Typography, useTheme, Button, Tabs } from "@mui/material";
 import RemoveIcon from "@mui/icons-material/Remove";
@@ -14,12 +14,47 @@ import Item from "../../components/item.jsx"
 
 
 const ItemDetails = () => {
-  return <div>Item Details</div>;
   const dispatch = useDispatch();
   const { itemId } = useParams();
-  //create value variable and corresponding setValue function. initial value of `value` is description.
   const [value, setValue] = useState("description");
+  const [count, setCount] = useState(1);
+  const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
+  async function getItem() {
+    //how you grab a single item
+    const item  = await fetch(
+      `http://localhost:2000/api/items?populate=image`,
+      {
+        method: "GET",
+      }
+    );
+    const itemJson = await item.json();
+    setItem(itemJson.data);
+  }
+  async function getItems() {
+    const items = await fetch(
+      `http://localhost:2000/api/items?populate=image`,
+      {
+        method: "GET",
+      }
+    );
+    const itemsJson = await items.json();
+    setItems(itemsJson.data);
+  }
+  //triggers functions when component mounts, or itemId changes
+  useEffect(()=>{
+    getItem();
+    getItems();
+  }, [itemId]);
 
+  return (
+  <div>Item Details</div>
+
+  )
 };
 
 export default ItemDetails;
