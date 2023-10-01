0c9857112a8bed3042cce52e8e170499693233f4
Natalie Benjes <natalie.benjes@gmail.com>
2023-10-01 16:41:29 -0700

    debug Item details image rendering

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -27,7 +27,7 @@ const ItemDetails = () => {
   async function getItem() {
     //how you grab a single item
     const item  = await fetch(
-      `http://localhost:2000/api/items?populate=image`,
+      `http://localhost:1337/api/items/${itemId}?populate=image`,
       {
         method: "GET",
       }
@@ -37,7 +37,7 @@ const ItemDetails = () => {
   }
   async function getItems() {
     const items = await fetch(
-      `http://localhost:2000/api/items?populate=image`,
+      `http://localhost:1337/api/items?populate=image`,
       {
         method: "GET",
       }
@@ -50,10 +50,57 @@ const ItemDetails = () => {
     getItem();
     getItems();
   }, [itemId]);
+console.log(item);
 
   return (
-  <div>Item Details</div>
+  <Box
+  width="80%" m="80px auto">
+    <Box display="flex" flexWrap="wrap" columnGap="40px">
+      {/* Images */}
+      <Box flex="1 1 40%" mb="40px">
+          <img
+            alt={item?.name}
+            width="100%"
+            height="100%"
+            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            style={{ objectFit: "contain" }}
+          />
+        </Box>
+        {/* Actions */}
+        <Box flex="1 1 50%" mb="40px">
+          <Box display="flex" justifyContent="space-between">
+            <Box>Home/Item</Box>
+            <Box>Prev/next</Box>
 
+          </Box>
+          {/* //top, right, bottom left */}
+          <Box m="65px 0 25px 0">
+            <Typography variant="h3">{item?.attributes?.shortDescription}</Typography>
+            <Typography>${item?.attributes?.price}</Typography>
+            <Typography sx={{ mt: "20px" }}>{item?.attributes?.longDescription}</Typography>
+          </Box>
+          <Box display="flex" alignItems="center" minHeight="50px">
+            <Box
+              display="flex"
+              alignItems="center"
+              border={`1.5px solid ${shades.secondary[300]}`}
+              mr="20px"
+              p="2px 5px"
+              >
+                {/* To add to cart: Could be a component */}
+                <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography sx={{ p: "0 5px"}}>{count}</Typography>
+              <IconButton onClick={() => setCount(count + 1)}>
+                <AddIcon />
+              </IconButton>
+              </Box>
+              <Button>Add to cart</Button>
+          </Box>
+        </Box>
+    </Box>
+    </Box>
   )
 };
 
