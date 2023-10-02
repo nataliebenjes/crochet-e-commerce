6fa0824361429cbd70e1676e8d023554e20010a9
Natalie Benjes <natalie.benjes@gmail.com>
2023-10-01 19:44:53 -0700

    WIP cart menu updating when user adds to cart

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from 'react-redux';
-import { IconButton, Box, Typography, useTheme, Button, Tabs } from "@mui/material";
+import { IconButton, Box, Typography, useTheme, Button, Tabs, Tab } from "@mui/material";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import { shades } from "../../theme.js";
 import { addToCart } from "../../state";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 //returns an object of key/value pairs of the dynamic params from  that were matched by the `<Route path>`
 import { useParams } from "react-router-dom";
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
@@ -96,12 +97,49 @@ console.log(item);
                 <AddIcon />
               </IconButton>
               </Box>
-              <Button>Add to cart</Button>
+              <Button
+              sx={{
+                backgroundColor: "pink"
+              }}
+              onClick={() => dispatch(addToCart({ item: { ...item, count }}))}>Add to cart</Button>
+          </Box>
+          <Box>
+            <Box m="20px 0 5px 0" display="flex">
+              <FavoriteBorderOutlinedIcon />
+              <Typography sx={{ ml: "5px"}}>Add to wishlist</Typography>
+            </Box>
+            <Typography>Category: {item?.attributes.category}</Typography>
           </Box>
         </Box>
     </Box>
+    {/* information */}
+    <Box m="20px 0">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="DESCRIPTION" value="description" />
+          <Tab label="REVIEWS" value="description" />
+        </Tabs>
     </Box>
+    <Box display="flex" flexWrap="wrap" gap="15px">
+      {value === "description" && (
+        <div>{item?.attributes?.longDescription}</div>
+      )}
+      {value === "reviews" && <div>reviews</div>}
+    </Box>
+        <Typography variant="h3" fontWeight="bold">Related Products</Typography>
+        <Box
+          mt="20px"
+          display="flex"
+          flexWrap="wrap"
+          columnGap="1.33%"
+          justifyContent="space-between"
+        >
+          {items.slice(0,4).map((item, i) => (
+            <Item key={`${item.name}-${i}`} item={item} width={undefined} />
+          ))}
+        </Box>
+        </Box>
   )
 };
+
 
 export default ItemDetails;
