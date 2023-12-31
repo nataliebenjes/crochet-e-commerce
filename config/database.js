8878f0e7d38175105732bb7e156dbfb1fd87eef2
Natalie Benjes <natalie.benjes@gmail.com>
2023-12-31 13:50:47 -0800

    simplify database.js

--- config/database.js
+++ config/database.js
@@ -2,13 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
       connection: {
-        connectionString: env('DATABASE_URL'),
+        // connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
+        port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        ssl: env.bool('DATABASE_SSL', true),
           // key: env('DATABASE_SSL_KEY', undefined),
           // cert: env('DATABASE_SSL_CERT', undefined),
           // ca: env('DATABASE_SSL_CA', undefined),
@@ -20,8 +20,7 @@ module.exports = ({ env }) => ({
           // ),
         },
       },
-    },
-});
+    });
 
     // mysql: {
     //   connection: {
