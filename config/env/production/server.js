39bafeb623f36832906b1c366b556480383d57f0
Natalie Benjes <natalie.benjes@gmail.com>
2024-01-06 14:17:03 -0800

    point server to render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,19 @@
 
+// module.exports = ({ env }) => ({
+//   proxy: true,
+//   url: env('APP_URL'), // Sets the public URL of the application.
+//   app: { 
+//     keys: env.array('APP_KEYS')
+//   },
+// });
+
 module.exports = ({ env }) => ({
   proxy: true,
   url: env('APP_URL'), // Sets the public URL of the application.
   app: { 
     keys: env.array('APP_KEYS')
+  },
+  database: {
+    host: env('DATABASE_HOST'),
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,10 +1,24 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
+// to run off hosted server. Need to figure out production script
+
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  proxy: true,
+  url: env('APP_URL'), // Sets the public URL of the application.
+  app: { 
+    keys: env.array('APP_KEYS')
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  database: {
+    host: env('DATABASE_HOST'),
   },
 });
